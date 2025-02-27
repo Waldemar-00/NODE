4e3de1cc360ff94ef92767312510924ce71ce68a
ULADZIMIR <01myit@gmail.com>
2025-02-27 19:19:20 +0300

    Module variables: global, performance, module and fn require.

---
+++ variables/module/mod.js
@@ -0,0 +1,6 @@
+console.log( __dirname ) //! C:\Users\for_i\Desktop\node_demo\variables\module
+console.log( __filename ) //! C:\Users\for_i\Desktop\node_demo\variables\module\mod.js
+console.log( global )
+console.log( performance )
+console.log( module )
+console.log( require )
