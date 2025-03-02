f298a1ffaa5b4e9d10f0f33dc2628bb869a30b46
ULADZIMIR <01myit@gmail.com>
2025-03-02 15:59:10 +0300

    Fixed worker_threads.

--- threads/worker.js
+++ threads/worker.js
@@ -1,27 +1,24 @@
 const { parentPort, workerData } = require( 'worker_threads' )
 const perf_hooks  = require( 'perf_hooks' )
 
-function toThreads ( { amount } )
+function toThreads ( amount  )
 {
-    function hardWork ( amount )
+    const array = []
+    for ( let i = 0; i <= amount; i++ )
     {
-        const array = []
-        for ( let i = 0; i <= amount; i++ )
-        {
-            array.unshift( i )
-        }
-        return array
+        array.unshift( i )
     }
-    hardWork = perf_hooks.performance.timerify( hardWork )
-    const observer = new perf_hooks.PerformanceObserver( ( items, observer ) =>
-    {
-        const entry = items.getEntriesByType( 'function' ).pop()
-        console.log( entry )
-        observer.disconnect()
-    } )
-    observer.observe( { entryTypes: [ 'function' ] } )
-    return hardWork ( amount )
+    console.log( array, "ARRAY", amount )
+    return array
 }
 
+toThreads = perf_hooks.performance.timerify( toThreads )
+const observer = new perf_hooks.PerformanceObserver( ( items, observer ) =>
+{
+    const entry = items.getEntriesByType( 'function' ).pop()
+    console.log( entry )
+    observer.disconnect()
+} )
+observer.observe( { entryTypes: [ 'function' ] } )
 
 parentPort.postMessage( toThreads( workerData ) )
